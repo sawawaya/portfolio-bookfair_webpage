15ca05201f6398951db537ec5852294ca9d2468a
sawawaya <terasawa.aya8290@gmail.com>
2023-07-17 06:34:55 +0900

    fix: 使用済みcouponの表示方法変更

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@
 
 {
     const coupon = document.querySelector('button');
+    const overlay = document.querySelector('.coupon-btn-overlay');
 
     coupon.addEventListener('click', () => {
-        coupon.parentNode.classList.add('coupon-btn');
+        // coupon.parentNode.classList.add('coupon-btn');
+        overlay.classList.add('show');
     });
 }
