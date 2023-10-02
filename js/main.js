4e9907cd5cdc9540565af58bae2b639835e74dc7
sawawaya <terasawa.aya8290@gmail.com>
2023-10-02 11:18:04 +0900

    クーポンクリック時の挙動変更

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@
 
     coupon.addEventListener('click', () => {
         // coupon.parentNode.classList.add('coupon-btn');
+        coupon.style.backgroundImage = 'none';
+        coupon.style.boxShadow = '0px 0px';
         overlay.classList.add('show');
     });
 }
